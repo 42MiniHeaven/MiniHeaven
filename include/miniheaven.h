fc53b3698f4c48187fb9ac80d9fc4bbb7d80c09a
Lukas Benedikt Wittwer <lwittwer@c3r8p7.42vienna.com>
2026-01-09 21:25:49 +0100

    updating headerfile and lukas files

--- include/miniheaven.h
+++ include/miniheaven.h
@@ -6,17 +6,25 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 17:23:25 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/08 16:00:47 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/01/09 21:13:49 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "miniheaven.h"
-# define "miniheaven.h"
+#ifndef MINIHEAVEN_H
+# define MINIHEAVEN_H
 
 # include <stdio.h>			//printf
 # include <unistd.h>		//write
 # include <stdlib.h>		//malloc
 # include <sys/types.h>		//pid_t
 # include <sys/wait.h>		//waitpid
+
+typedef struct	s_exec
+{
+	int		args;
+	int		*status;
+	pid_t	*process;
+	int		pipefd[2];
+}	t_exec;
 
 #endif
--- src/Lukas/forking.c
+++ src/Lukas/forking.c
@@ -6,50 +6,96 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 18:27:03 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/08 16:30:31 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/01/09 19:32:36 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/miniheaven.h"
+#include <string.h>
 
-static void	child(int nb)
+static void	child(int nb, int read_fd, int write_fd, int is_last)
 {
-	printf("Hello i am child number: %i\n", nb);
-	fflush(stdout);
+	char	buffer[1024] = {0};
+
+	if(read_fd != -1)
+	{
+		read(read_fd, buffer, sizeof(buffer));
+		close(read_fd);
+	}
+	char mynum[32];
+	snprintf(mynum, sizeof(mynum), "%d", nb);
+	strncat(buffer, mynum, sizeof(buffer) - strlen(buffer) - 1);
+	if(is_last)
+	{
+		printf("Final output: %s\n", buffer);
+		fflush(stdout);
+	}
+	else
+	{
+		write(write_fd, buffer, strlen(buffer) + 1);
+	}
+	close(write_fd);
 	exit(0);
 }
 
-static pid_t	create_fork(int nb)
+static pid_t	create_fork(int nb, int read_fd, int write_fd, int is_last)
 {
 	pid_t	c;
 
 	c = fork();
 	if (c == 0)
-		child(nb);
+		child(nb, read_fd, write_fd, is_last);
 	return (c);
 }
 
-int	forking(int children)
+int	forking(t_exec *test)
 {
-	int		status[children];
-	pid_t	process[children];
+	int	prev_read = -1;
 	int	i = 0;
-	while(i < children)
+	while(i < test->args)
 	{
-		process[i] = create_fork(i);
+		if(i < test->args - 1)
+		{
+			if(pipe(test->pipefd) == -1)
+				exit(2);
+		}
+		else
+		{
+			test->pipefd[0] = -1;
+			test->pipefd[1] = -1;
+		}
+		test->process[i] = create_fork(i, prev_read, test->pipefd[1], i == test->args - 2);
+		if (prev_read != -1)
+			close(prev_read);
+		if (test->pipefd[1] != -1)
+			close(test->pipefd[1]);
+		prev_read = test->pipefd[0];
 		i++;
 	}
 	i = 0;
-	while(i < children)
+	while(i < test->args)
 	{
-		waitpid(process[i], &status[i], 0);
+		waitpid(test->process[i], &test->status[i], 0);
 		i++;
 	}
 	return (0);
 }
 
-int	main()
+int	main(int argc, char **argv)
 {
-	forking(3);
+	t_exec	test;
+	int	size;
+
+	if(argc == 1)
+		exit(1);	
+	size = atoi(argv[1]);
+	if(argc > 0)
+	{
+		test.args = size;
+		test.status = malloc(size * sizeof(int));
+		test.process = malloc(size * sizeof(pid_t));
+		forking(&test);
+	}
+	//free
 	return(0);
 }
