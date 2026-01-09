066a7ef1cebbef8fa63f96c906627ef145d143be
Agata Julia Zielnica <azielnic@c3r8p8.42vienna.com>
2026-01-09 22:25:55 +0100

    Played with adding structs for parsing - not complete

--- include/miniheaven.h
+++ include/miniheaven.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniheaven.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
+/*   By: azielnic <azielnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 17:23:25 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/09 21:13:49 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/01/09 22:15:34 by azielnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,30 @@
 # include <stdlib.h>		//malloc
 # include <sys/types.h>		//pid_t
 # include <sys/wait.h>		//waitpid
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// idea: use enums for specific operands
+
+// typedef enum operands
+// {
+	
+// } ;
+
+typedef struct	s_tokens
+{
+	char *token;
+	t_tokens *next; // check again how linked lists work
+
+}	t_tokens;
+
+typedef struct	s_parser
+{
+	char *line;
+//	char *history; // maybe an array maybe a file maybe name of file 
+	char *	
+
+}	t_parser;
 
 typedef struct	s_exec
 {
@@ -25,6 +49,7 @@ typedef struct	s_exec
 	int		*status;
 	pid_t	*process;
 	int		pipefd[2];
+	t_parser	
 }	t_exec;
 
 #endif
