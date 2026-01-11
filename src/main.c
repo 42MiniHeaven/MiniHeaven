40b659527136367c7033a101046baafaf8ef6eb0
Agata Julia Zielnica <azielnic@c3r8p5.42vienna.com>
2026-01-11 21:32:43 +0100

    Minor adaptations to main incl comments and ctrl+\ signal ignore

--- src/main.c
+++ src/main.c
@@ -3,32 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
+/*   By: azielnic <azielnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 16:59:07 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/04 18:18:48 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/01/11 21:31:22 by azielnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <stdlib.h>
 
-int	main(void)
+/* 
+ * GENERAL:
+ * 		The main currently displays a prompt and reads input. 
+ * 
+ * IMPLEMENTED:		
+ * 		-readline 
+ * 		-history
+ * 		-ctrl-\ (should do nothing according to the subject)
+ * 
+ * TO BE IMPLEMENTED:
+ * 		-exit builtin
+ * 		-ctrl+d handling (needs its own signal handler)
+ * 		-ctrl+c handling (needs its own signal handler)
+ */
+
+int	main(int argc, char **argv, char **envp)
 {
-	char	*in; //input string
+	char	*input; //input string
+
+	(void) argc;
+	(void) argv;
+	(void) envp;
+	signal(SIGQUIT, SIG_IGN); // ctrl-\ signal is ignored
 	while(1)
 	{
-		in = readline("MiniHeaven> ");
-		if (!in)
+		input = readline("MiniHeaven> ");
+		if (!input)
 		{
-			free(in); //dont know if we need this
-			exit(0);
+			break;
+			//exit(0);
 		}
-		if (in)
-			printf("%s\n", in); //pass to parser
-		free(in);
+		if (input)
+		{
+			printf("%s", input); //pass to parser
+			if (input[0] != '\0')
+			{
+				printf("\n");
+				add_history(input);
+			}
+		}
+		free(input);
 	}
 	return (0);
 }
