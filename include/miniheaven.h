2116b18f7715c3c88da79b851dfa78afc73d7b73
Agata Julia Zielnica <azielnic@c1r5p5.42vienna.com>
2026-01-18 22:30:26 +0100

    adapted miniheaven header file

--- include/miniheaven.h
+++ include/miniheaven.h
@@ -6,42 +6,30 @@
 /*   By: azielnic <azielnic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 17:23:25 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/09 22:15:34 by azielnic         ###   ########.fr       */
+/*   Updated: 2026/01/18 19:36:37 by azielnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIHEAVEN_H
 # define MINIHEAVEN_H
 
-# include <stdio.h>			//printf
-# include <unistd.h>		//write
-# include <stdlib.h>		//malloc
-# include <sys/types.h>		//pid_t
-# include <sys/wait.h>		//waitpid
+# include <stdio.h>					//printf
+# include <unistd.h>				//write
+# include <stdlib.h>				//malloc
+# include <sys/types.h>				//pid_t
+# include <sys/wait.h>				//waitpid
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "parser.h"
 
-// idea: use enums for specific operands
-
-// typedef enum operands
-// {
-	
-// } ;
-
-typedef struct	s_tokens
+// main structure comtaining everything, which also can be freed
+typedef struct	s_heaven
 {
-	char *token;
-	t_tokens *next; // check again how linked lists work
-
-}	t_tokens;
-
-typedef struct	s_parser
-{
-	char *line;
-//	char *history; // maybe an array maybe a file maybe name of file 
-	char *	
-
-}	t_parser;
+	t_token *lexer;
+	t_cmd	*cmd_table;
+	int		exit_code;
+	char	**envp
+}	t_heaven;
 
 typedef struct	s_exec
 {
