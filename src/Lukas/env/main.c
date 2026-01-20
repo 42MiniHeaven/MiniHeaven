bde81287336615035edbaa47232b9e4528e51f91
Lukas Benedikt Wittwer <lwittwer@c3r8p7.42vienna.com>
2026-01-20 20:57:59 +0100

    for testing

--- src/Lukas/env/main.c
+++ src/Lukas/env/main.c
@@ -6,11 +6,28 @@
 /*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 16:07:00 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/01/19 16:07:59 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/01/20 20:56:06 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env.h"
+
+void	create_env(char **envp)
+{
+	t_env	start = NULL;
+	t_env	tmp;
+	size_t	i = 0;
+	char	*key;
+	char	*value;
+
+	while (envp[i])
+	{
+		key = ft_substr();
+		value = ft_substr();
+		tmp = env_new(key, value);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
