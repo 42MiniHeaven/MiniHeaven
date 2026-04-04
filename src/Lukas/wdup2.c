c66c1fb41c19d9e848ac015e2b77d29934cd5a9e
Lukas Benedikt Wittwer <lwittwer@c1r4p2.42vienna.com>
2026-04-04 18:25:22 +0200

    cleanup

--- src/Lukas/wdup2.c
+++ src/Lukas/wdup2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wdup2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
+/*   By: lwittwer <lwittwer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 11:16:15 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/03/30 18:45:14 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/04/04 18:20:02 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,4 @@ void	wdup2(int oldfd, int newfd)
 {
 	if (dup2(oldfd, newfd) == -1)
 		fatal("dup2", 1);
-	printf("entered\n");
 }
