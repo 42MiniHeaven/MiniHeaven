e4ac699cefb5e33f1efe1b691de88e4ffd1f9dca
Lukas Benedikt Wittwer <lwittwer@c2r7p6.42vienna.com>
2026-04-06 20:58:59 +0200

    fixed the crash

--- src/Lukas/execute.c
+++ src/Lukas/execute.c
@@ -6,7 +6,7 @@
 /*   By: lwittwer <lwittwer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 21:10:10 by lwittwer          #+#    #+#             */
-/*   Updated: 2026/04/06 19:33:43 by lwittwer         ###   ########.fr       */
+/*   Updated: 2026/04/06 20:55:54 by lwittwer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	execute(t_shell *data)
 {
 	if (!data || !data->cmds)
 		return (0);
-	if (data->cmds->argv && ft_strcmp(data->cmds->argv[0], "exit") && data->cmds->next == NULL)
+	if (data->cmds->argv && data->cmds->argv[0] 
+		&& ft_strcmp(data->cmds->argv[0], "exit") && data->cmds->next == NULL)
 		data->should_exit = 1;
 	if (!data->cmds->next)
 		return (exec_single(data));
