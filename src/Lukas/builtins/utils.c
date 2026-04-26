a699d64314388c9afa7c1a9b2ba109a388adcedd
Lukas Benedikt Wittwer <lwittwer@c1r4p2.42vienna.com>
2026-04-26 14:25:27 +0200

    add: utils.c

---
+++ src/Lukas/builtins/utils.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwittwer <lwittwer@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/26 13:09:13 by lwittwer          #+#    #+#             */
+/*   Updated: 2026/04/26 13:11:49 by lwittwer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniheaven.h"
+
+void	ft_err_export_three(char *func, char *error, char *extra)
+{
+	char	*first;
+	char	*second;
+	char	*third;
+
+	first = ft_strjoin("minishell: ", func);
+	if (!first)
+		return ;
+	second = ft_strjoin(first, error);
+	if (!second)
+		return (free(first));
+	third = ft_strjoin_three(second, "': ", extra);
+	if (!third)
+		return (free(first), free(second));
+	ft_putendl_fd(third, 2);
+	free(first);
+	free(second);
+	free(third);
+}
